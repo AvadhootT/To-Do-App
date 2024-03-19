4539afeed4beb22d34f8bc16380999adc46defd8
AvadhootT <95272487+AvadhootT@users.noreply.github.com>
2024-03-19 14:40:25 +0530

    solved login issue -> was taking name also along with email and password, but the code accepts email and password
    added next parameter in the myprofile handler

--- controllers/user.js
+++ controllers/user.js
@@ -30,31 +30,35 @@ export const register = async (req, res, next) => {
 };
 
 
-export const login = async (req, res, next)=>{
-
+export const login = async (req, res, next) => {
     try {
-        const {name, email, password} = req.body;
+        const { email, password } = req.body;
+        console.log("Received login request:", { email, password });
 
-        const user = await User.findOne({email}).select("+password");
-    
-        if(!user) 
-            return next(new ErrorHandler("Invalid email or password", 404))
-    
-    
+        const user = await User.findOne({ email }).select("+password");
+
+        if (!user) {
+            console.log("User not found for email:", email);
+            return next(new ErrorHandler("Invalid email or password", 404));
+        }
+
         const isMatch = await bcrypt.compare(password, user.password);
-    
-        if(!isMatch) 
-            return next(new ErrorHandler("Invalid email or password", 404))
-    
+        console.log("Password match:", isMatch);
+
+        if (!isMatch) {
+            console.log("Password does not match");
+            return next(new ErrorHandler("Invalid email or password", 404));
+        }
+
         sendCookies(user, res, 200, `Logged In Successfully -> Welcome back ${user.name}`);
     } catch (error) {
+        console.error("Login error:", error);
         next(error);
     }
+};
 
-}
 
-
-export const getMyProfile =  async (req, res)=>{
+export const getMyProfile =  async (req, res, next)=>{
 
     // res.cookie() -> token -> id -> extract krun -> database mdhe find krun -> return karaycha
     try {
